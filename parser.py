0e107206b82790b76e176869545ed06c613bd7f7
John Lin <jlin22@stuy.edu>
2018-03-12 11:00:15 -0400

    stuff

--- parser.py
+++ parser.py
@@ -109,3 +109,24 @@ def parse_file( fname, edges, transform, screen, color ):
             add_curve(edges, float(args[0]), float(args[1]), float(args[2]), float(args[3]), 
             float(args[4]), float(args[5]), float(args[6]), float(args[7]), 0.001, 'bezier')
         c+= 1
+
+screen = new_screen()
+color = [ 0, 255, 0 ]
+edges = []
+transform = new_matrix()
+'''add_circle(edges, 250, 250, 0, 100, 0.01)
+add_curve(edges, 100, 100, 200, 400, 300, 100, 400, 400, 0.01, 'bezier')
+print_matrix(edges)
+draw_lines(edges, screen, color)
+save_ppm(screen, 'img.ppm')'''
+# print_matrix( make_translate(3, 4, 5) )
+# print
+# print_matrix( make_scale(3, 4, 5) )
+# print
+# print_matrix( make_rotX(math.pi/4) )
+# print
+# print_matrix( make_rotY(math.pi/4) )
+# print
+# print_matrix( make_rotZ(math.pi/4) )
+
+parse_file( 'script', edges, transform, screen, color )
